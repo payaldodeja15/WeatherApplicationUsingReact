20b5508be083d960df92970909d98681fd0ca0aa
payaldodeja15 <dodeja.p@husky.neu.edu>
2018-04-28 02:28:01 -0400

    using stateless

--- weather-app-using-react/src/components/Weather.js
+++ weather-app-using-react/src/components/Weather.js
@@ -1,17 +1,40 @@
 import React from "react";
 
-class Weather extends React.Component{
-    render()
-    {
-        return(
+//for stateless
+const Weather = (props) =>{
+    return(
+        <div>
+                        {props.city && props.country && <p>  Location:{props.city}, Country:{props.country}</p>}
+        
+                       {props.temperature && <p>Temperature:  {props.temperature}</p>}
+                     
+                       {props.humidity && <p> Humidity: {props.humidity}</p>}
+                       {props.description && <p> Conditions:  {props.description}</p>}
+                         {props.error && <p>{props.error}</p>}
+                          </div>
+    );
+}
 
-            <div>
-                
-                {this.props.temperature}
-                
-                 </div>
-        );
-    }
-};
+
+
+
+
+// class Weather extends React.Component{
+//     render()
+//     {
+//         return(
+
+//             <div>
+//                 {this.props.city && this.props.country && <p>  Location:{this.props.city}, Country:{this.props.country}</p>}
+
+//               {this.props.temperature && <p>Temperature:  {this.props.temperature}</p>}
+             
+//               {this.props.humidity && <p> Humidity: {this.props.humidity}</p>}
+//               {this.props.description && <p> Conditions:  {this.props.description}</p>}
+//                 {this.props.error && <p>{this.props.error}</p>}
+//                  </div>
+//         );
+//     }
+// };
 
 export default Weather;
